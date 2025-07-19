cf37c5a9a73a9bf389fbeb2c6de55489e95d6a6c
camhpj <camhpjohnson@gmail.com>
2025-07-18 21:24:53 -0400

    delete old code and move some functions around

--- src/data.py
+++ src/data.py
@@ -2,10 +2,6 @@ from typing import (
     Any,
     Dict,
     Generator,
-    Literal,
-    Optional,
-    OrderedDict,
-    Sequence,
     Tuple,
 )
 
@@ -13,92 +9,12 @@ import cv2
 import numpy as np
 import SimpleITK as sitk
 
-
-def process_nrrd_metadata(metadata: OrderedDict[str, Any]) -> Dict[str, Any]:
-    """Standardize nrrd metadata. Specific for AAA dataset."""
-    size = metadata["sizes"][::-1].tolist()
-    dimension = metadata["dimension"]
-    origin: np.ndarray = metadata["space origin"]
-    directions: np.ndarray = metadata["space directions"]
-    spacing: np.ndarray = np.linalg.norm(directions, axis=0)
-    directions_norm = directions / spacing
-    return {
-        "size": size,
-        "spacing": spacing,
-        "origin": origin,
-        "direction": directions_norm,
-        "dimension": dimension,
-    }
-
-
-def create_itk_image_from_array(
-        arr: np.ndarray,
-        origin: Tuple[float, float, float],
-        spacing: Tuple[float, float, float],
-        direction: Tuple[float, float, float, float, float, float, float, float, float],
-    ) -> sitk.Image:
-    # convert ndarray into itk Image() class instance
-    img = sitk.GetImageFromArray(arr)
-    img.SetOrigin(origin)
-    img.SetSpacing(spacing)
-    img.SetDirection(direction)
-    return img
-
-
-def create_itk_resampler(
-        origin: Tuple[float, float, float],
-        direction: Tuple[float, float, float, float, float, float, float, float, float],
-        new_spacing: Tuple[float, float, float],
-        new_size: Tuple[int, int, int],
-        default_pixel_value: int,
-        interpolation: Literal["linear", "nn"],
-    ) -> sitk.ResampleImageFilter:
-    # itk resampler
-    resampler = sitk.ResampleImageFilter()
-    resampler.SetOutputSpacing(new_spacing)
-    resampler.SetSize(new_size)
-
-    resampler.SetOutputOrigin(origin)
-    resampler.SetOutputDirection(direction)
-    resampler.SetDefaultPixelValue(default_pixel_value)
-    match interpolation:
-        case "linear":
-            resampler.SetInterpolator(sitk.sitkLinear)
-        case "nn":
-            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-        case _:
-            raise ValueError(f"Invalid interpolation method {interpolation}.")
-    return resampler
-
-
-def get_contours(
-    img: np.ndarray,
-    thresh_kwargs: Optional[Dict[str, Any]] = None,
-    contour_kwargs: Optional[Dict[str, Any]] = None,
-) -> Sequence[cv2.typing.MatLike]:
-    """Get contours in image.
-
-    Args:
-        img (np.ndarray): 2D image.
-        thresh_kwargs (Optional[Dict[str, Any]], optional): Keyword arguments for cv2.threshold() . Defaults to None.
-        contour_kwargs (Optional[Dict[str, Any]], optional): Keyword arguments for cv2.findcontour(). Defaults to None.
-
-    Returns:
-        Sequence[cv2.typing.MatLike]: Sequence of contours.
-    """
-    if not thresh_kwargs:
-        thresh_kwargs = {"thresh": 1, "maxval": 255, "type": cv2.THRESH_BINARY}
-    if not contour_kwargs:
-        contour_kwargs = {"mode": cv2.RETR_EXTERNAL, "method": cv2.CHAIN_APPROX_SIMPLE}
-    _, thresh = cv2.threshold(img, **thresh_kwargs)
-    contours, _ = cv2.findContours(thresh, **contour_kwargs)
-    return contours
-
-
-def get_largest_contour(contours: Sequence[cv2.typing.MatLike]) -> cv2.typing.MatLike:
-    """Return the contours with the largest area."""
-    largest_contour = max(contours, key=cv2.contourArea)
-    return largest_contour
+from src.preprocessing import (
+    create_itk_image_from_array,
+    create_itk_resampler,
+    get_contours,
+    get_largest_contour,
+)
 
 
 class AxialSlice:
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,40 +1,92 @@
-from typing import Tuple
+from typing import Any, Dict, Literal, Optional, OrderedDict, Sequence, Tuple
 
 import cv2
 import numpy as np
+import SimpleITK as sitk
 
 
-def window_ct(
-    img: np.ndarray, window: int, level: int, rescale: bool = True
-) -> np.ndarray[np.uint8]:
-    img_min = level - window // 2
-    img_max = level + window // 2
-    img[img < img_min] = img_min
-    img[img > img_max] = img_max
-    if rescale:
-        img = (img - img_min) / (img_max - img_min) * 255.0
-    return img.astype(np.uint8)
+def process_nrrd_metadata(metadata: OrderedDict[str, Any]) -> Dict[str, Any]:
+    """Standardize nrrd metadata. Specific for AAA dataset."""
+    size = metadata["sizes"][::-1].tolist()
+    dimension = metadata["dimension"]
+    origin: np.ndarray = metadata["space origin"]
+    directions: np.ndarray = metadata["space directions"]
+    spacing: np.ndarray = np.linalg.norm(directions, axis=0)
+    directions_norm = directions / spacing
+    return {
+        "size": size,
+        "spacing": spacing,
+        "origin": origin,
+        "direction": directions_norm,
+        "dimension": dimension,
+    }
 
 
-def normalize_ct_slice(
-    img: np.ndarray, mean: float, stdev: float
-) -> np.ndarray[np.float32]:
-    img = img - mean / stdev
-    return img.astype(np.float32)
+def create_itk_image_from_array(
+        arr: np.ndarray,
+        origin: Tuple[float, float, float],
+        spacing: Tuple[float, float, float],
+        direction: Tuple[float, float, float, float, float, float, float, float, float],
+    ) -> sitk.Image:
+    # convert ndarray into itk Image() class instance
+    img = sitk.GetImageFromArray(arr)
+    img.SetOrigin(origin)
+    img.SetSpacing(spacing)
+    img.SetDirection(direction)
+    return img
 
 
-def find_ct_bounding_rect(img: np.ndarray) -> Tuple[int, int, int, int]:
-    _, thresh = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def create_itk_resampler(
+        origin: Tuple[float, float, float],
+        direction: Tuple[float, float, float, float, float, float, float, float, float],
+        new_spacing: Tuple[float, float, float],
+        new_size: Tuple[int, int, int],
+        default_pixel_value: int,
+        interpolation: Literal["linear", "nn"],
+    ) -> sitk.ResampleImageFilter:
+    # itk resampler
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetOutputSpacing(new_spacing)
+    resampler.SetSize(new_size)
+
+    resampler.SetOutputOrigin(origin)
+    resampler.SetOutputDirection(direction)
+    resampler.SetDefaultPixelValue(default_pixel_value)
+    match interpolation:
+        case "linear":
+            resampler.SetInterpolator(sitk.sitkLinear)
+        case "nn":
+            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+        case _:
+            raise ValueError(f"Invalid interpolation method {interpolation}.")
+    return resampler
+
+
+def get_contours(
+    img: np.ndarray,
+    thresh_kwargs: Optional[Dict[str, Any]] = None,
+    contour_kwargs: Optional[Dict[str, Any]] = None,
+) -> Sequence[cv2.typing.MatLike]:
+    """Get contours in image.
+
+    Args:
+        img (np.ndarray): 2D image.
+        thresh_kwargs (Optional[Dict[str, Any]], optional): Keyword arguments for cv2.threshold() . Defaults to None.
+        contour_kwargs (Optional[Dict[str, Any]], optional): Keyword arguments for cv2.findcontour(). Defaults to None.
+
+    Returns:
+        Sequence[cv2.typing.MatLike]: Sequence of contours.
+    """
+    if not thresh_kwargs:
+        thresh_kwargs = {"thresh": 1, "maxval": 255, "type": cv2.THRESH_BINARY}
+    if not contour_kwargs:
+        contour_kwargs = {"mode": cv2.RETR_EXTERNAL, "method": cv2.CHAIN_APPROX_SIMPLE}
+    _, thresh = cv2.threshold(img, **thresh_kwargs)
+    contours, _ = cv2.findContours(thresh, **contour_kwargs)
+    return contours
+
+
+def get_largest_contour(contours: Sequence[cv2.typing.MatLike]) -> cv2.typing.MatLike:
+    """Return the contours with the largest area."""
     largest_contour = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(largest_contour)
-    return (x, y, w, h)
-
-
-def find_ct_outer_contour_mask(img: np.ndarray) -> None:
-    _, thresh = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    largest_contour = max(contours, key=cv2.contourArea)
-    mask = np.zeros(img.shape, np.uint8)
-    cv2.drawContours(mask, [largest_contour], -1, (255), thickness=cv2.FILLED)
-    return mask
+    return largest_contour
